68dc6831a237dfc1444044d8591fce76ff54aaa0
info-4 <info-4@INFO4-PC1>
2022-06-15 08:54:44 -0300

    Agregar archivos de proyecto.

---
+++ Página Blazor Entity Framework/Data/WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace Página_Blazor_Entity_Framework.Data
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
